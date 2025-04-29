695ca5136ce9f82f17bf5a3c80b779e6b0459109
Jerrychenjikai <jerry_chenjikai@hotmail.com>
2025-04-28 20:52:46 -0400

    file rename

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "node.h"
-#include "binaryheap.h"
+#include "radixheap.h"
 #include <vector>
 #include <cstdio>
 #include <chrono>
@@ -30,7 +30,7 @@ priorityq q;
 
 int main()
 {
-	freopen("c.in","r",stdin);
+	freopen("1000-10000.in","r",stdin);
 	scanf("%d %d %d",&n, &m, &root);
 	
 	for(int i=0;i<=n;i++) nodes[i].id=i;
